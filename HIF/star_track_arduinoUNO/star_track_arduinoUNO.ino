fc7acb728cfd668c39aa26b65cd020378ef72f8d
HOZAIFA SHAKEEL <112809008+hozaifa56@users.noreply.github.com>
2023-12-02 12:53:17 +0530

    2 december commit

--- HIF/star_track_arduinoUNO/star_track_arduinoUNO.ino
+++ HIF/star_track_arduinoUNO/star_track_arduinoUNO.ino
@@ -1,11 +1,4 @@
-/*
- This is the source code for the master-module of Star Track.
- Required Libraries:
- https://virtuabotix-virtuabotixllc.netdna-ssl.com...
- https://github.com/jrowberg/i2cdevlib/zipball/mas...
 
- Created 20 July 2016 by Görkem Bozkurt
- */
 #include <virtuabotixRTC.h>
 #include <Wire.h>
 #include <MPU6050.h>
@@ -14,7 +7,8 @@ MPU6050 mpu;
 virtuabotixRTC myRTC(A0, A1, A2);
 double M,Y,D,MN,H,S;
 double A,B;
-double location =32.88;//your longtitude
+double location =80.94;//your longtitude.
+double location2 =26.84;//your latitude.
 double LST_degrees;//variable to store local side real time(LST) in degrees.
 double LST_hours;//variable to store local side real time(LST) in decimal hours.
 unsigned long timer = 0;
@@ -25,13 +19,15 @@ double yaw = 0;
 double val = 0;//variable to store the user input DEC
 double val2 = 0;//variable to store the user input RA
 double temp = val2;//temporary value to store val2
-const int stahp=7,stahp2=10;
-const int cw=8,cw2=11;
-const int ccw=6,ccw2=9;
+const int stahp=6,stahp2=9;
+const int cw=7,cw2=10;
+const int ccw=8,ccw2=11;
+bool receivedCoordinates = false; // Flag to track if coordinates are received
 void setup() {
     //set date-time according to (seconds, minutes, hours, day of the week, day of the month, month, year) 
-    myRTC.setDS1302Time(00, 38, 23, 5, 27, 7, 2016);
-    Serial.begin(115200);
+    myRTC.setDS1302Time(00, 45, 10, 2, 11, 11, 2023);
+    Wire.begin();
+    Serial.begin(9600);
     pinMode(stahp,OUTPUT);
     pinMode(cw,OUTPUT);
     pinMode(ccw,OUTPUT);
@@ -49,17 +45,29 @@ void loop()
 {
     //this will update the RA degrees with sidereal time 1degree at a time
     //this way the object or star on the sky is tracked.
+  if(location2>0){//for the northern hemisphere. 
     if( floor(LST_degrees)==LST_degrees ){ 
       if (LST_degrees>180){
         val2 = temp+(360-LST_degrees);
         }else{
-        val2 = temp-LST_degrees;
+        val2 = temp-LST_degrees; //use val2 = temp+LST_degrees; if you are located in the southern hemisphere.
         }
     }
-
+  }else{//for the southern hemisphere.
+   if( floor(LST_degrees)==LST_degrees ){ 
+      if (LST_degrees>180){
+        val2 = temp-(360-LST_degrees);
+        }else{
+        val2 = temp+LST_degrees; //use val2 = temp+LST_degrees; if you are located in the southern hemisphere.
+        }
+    }
+  }
     myRTC.updateTime();
     LST_time();
-    recvdata();
+    if (!receivedCoordinates) {
+        recvdata(); // Receive coordinates only once
+        receivedCoordinates = true; // Set the flag to true after receiving coordinates
+    }
     pitch_check();
     yaw_check();
     timer = millis();
@@ -81,12 +89,70 @@ void loop()
     Serial.println(LST_hours);//local sidereal time in decimal hours.
     delay((timeStep*1000) - (millis() - timer));//timer for the gyro.
 }
+// void recvdata() {
+//   Serial.println("Enter the coordinates (format: x,y):");
+
+//   while (!Serial.available()) {
+//     // Wait for input
+//   }
+
+//   String input = Serial.readStringUntil('\n'); // Read input until newline character
+//   input.trim(); // Remove leading/trailing whitespace
+
+//   int commaIndex = input.indexOf(',');
+  
+//   if (commaIndex != -1) { // Check if the comma is found in the input
+//     String xStr = input.substring(0, commaIndex); // Extract the x-coordinate substring
+//     String yStr = input.substring(commaIndex + 1); // Extract the y-coordinate substring
+
+//     xStr.trim(); // Remove whitespace
+//     yStr.trim(); // Remove whitespace
+
+//     val = 90-xStr.toDouble(); // Convert x-coordinate string to double
+//     val2 = yStr.toDouble(); // Convert y-coordinate string to double
+//     temp=val2;
+
+//     Serial.print("Received coordinates: ");
+//     Serial.print(val);
+//     Serial.print(", ");
+//     Serial.println(val2);
+
+//     // Store the values in global variables or use them as needed
+//     // val and val2 now hold the double values of the coordinates
+//   } else {
+//     Serial.println("Invalid input format. Please enter coordinates in the format: x,y");
+//   }
+// }
+
 void recvdata(){
   //This function receives data from serial as (0.00,0.00)
   //splits it to strings by the comma ","
   //than converts them to doubles 
-    if (Serial.available() > 0){
-        String a= Serial.readString();
+    // if (Serial.available() > 0){
+    //     Serial.println("Enter the coordinates:");
+    //     String a= Serial.readString();
+    //     String value1, value2;
+    //     // For loop which will separate the String in parts
+    //     // and assign them the the variables we declare
+    //     for (int i = 0; i < a.length(); i++) {
+    //         if (a.substring(i, i+1) == ",") {
+    //             value2 = a.substring(0, i);
+    //             value1= a.substring(i+1);
+    //             break;
+    //         }
+    //     }
+    //     val=90-value1.toFloat();
+    //     val2=value2.toFloat();
+    //     temp = val2;
+    //     Serial.println("the coordinates:");
+    //     Serial.println(val);
+    //     Serial.println(val2);
+    // }
+    Serial.println("Enter the coordinates:");
+    while (!Serial.available()) {
+      // Wait for input
+    }
+    String a= Serial.readString();
         String value1, value2;
         // For loop which will separate the String in parts
         // and assign them the the variables we declare
@@ -97,14 +163,19 @@ void recvdata(){
                 break;
             }
         }
+        Serial.println(val);
+        Serial.println(val2);
         val=90-value1.toFloat();
         val2=value2.toFloat();
         temp = val2;
-    }
+        Serial.println("the coordinates:");
+        Serial.println(val);
+        Serial.println(temp);
 }
 void pitch_check(){
     //check if pitch is high, low or equal to the user input
     //send commands to slave-module to start and stop motors
+    Serial.println("i am in pitch_check function");
     if(floor(pitch*100)/100==floor(val*100)/100){
         digitalWrite(stahp,HIGH);
         }else{
@@ -124,6 +195,7 @@ void pitch_check(){
 void yaw_check(){
     //check if yaw is high, low or equal to the user input
     //send commands to slave-module to start and stop motors
+    Serial.println("i am in yaw_check function");
     if(floor(yaw*100)==floor(val2*100)){
         digitalWrite(stahp2,HIGH);
         }else{
--- HIF/stepper_basic/stepper_basic.ino
+++ HIF/stepper_basic/stepper_basic.ino
@@ -1,14 +1,22 @@
 //Written By Nikodem Bartnik - nikodembartnik.pl
-#define STEPPER_PIN_1 9
-#define STEPPER_PIN_2 10
-#define STEPPER_PIN_3 11
-#define STEPPER_PIN_4 12
+#define STEPPER_PIN_1 A0
+#define STEPPER_PIN_2 A1
+#define STEPPER_PIN_3 A2
+#define STEPPER_PIN_4 A3
+#define STEPPER_PIN_5 6
+#define STEPPER_PIN_6 7
+#define STEPPER_PIN_7 8
+#define STEPPER_PIN_8 9
 int step_number = 0;
 void setup() {
 pinMode(STEPPER_PIN_1, OUTPUT);
 pinMode(STEPPER_PIN_2, OUTPUT);
 pinMode(STEPPER_PIN_3, OUTPUT);
 pinMode(STEPPER_PIN_4, OUTPUT);
+pinMode(STEPPER_PIN_5, OUTPUT);
+pinMode(STEPPER_PIN_6, OUTPUT);
+pinMode(STEPPER_PIN_7, OUTPUT);
+pinMode(STEPPER_PIN_8, OUTPUT);
 
 }
 
@@ -29,24 +37,40 @@ switch(step_number){
   digitalWrite(STEPPER_PIN_2, LOW);
   digitalWrite(STEPPER_PIN_3, LOW);
   digitalWrite(STEPPER_PIN_4, LOW);
+  digitalWrite(STEPPER_PIN_5, HIGH);
+  digitalWrite(STEPPER_PIN_6, LOW);
+  digitalWrite(STEPPER_PIN_7, LOW);
+  digitalWrite(STEPPER_PIN_8, LOW);
   break;
   case 1:
   digitalWrite(STEPPER_PIN_1, LOW);
   digitalWrite(STEPPER_PIN_2, HIGH);
   digitalWrite(STEPPER_PIN_3, LOW);
   digitalWrite(STEPPER_PIN_4, LOW);
+  digitalWrite(STEPPER_PIN_5, LOW);
+  digitalWrite(STEPPER_PIN_6, HIGH);
+  digitalWrite(STEPPER_PIN_7, LOW);
+  digitalWrite(STEPPER_PIN_8, LOW);
   break;
   case 2:
   digitalWrite(STEPPER_PIN_1, LOW);
   digitalWrite(STEPPER_PIN_2, LOW);
   digitalWrite(STEPPER_PIN_3, HIGH);
   digitalWrite(STEPPER_PIN_4, LOW);
+  digitalWrite(STEPPER_PIN_5, LOW);
+  digitalWrite(STEPPER_PIN_6, LOW);
+  digitalWrite(STEPPER_PIN_7, HIGH);
+  digitalWrite(STEPPER_PIN_8, LOW);
   break;
   case 3:
   digitalWrite(STEPPER_PIN_1, LOW);
   digitalWrite(STEPPER_PIN_2, LOW);
   digitalWrite(STEPPER_PIN_3, LOW);
   digitalWrite(STEPPER_PIN_4, HIGH);
+  digitalWrite(STEPPER_PIN_5, LOW);
+  digitalWrite(STEPPER_PIN_6, LOW);
+  digitalWrite(STEPPER_PIN_7, LOW);
+  digitalWrite(STEPPER_PIN_8, HIGH);
   break;
 } 
   }else{
@@ -56,25 +80,40 @@ switch(step_number){
   digitalWrite(STEPPER_PIN_2, LOW);
   digitalWrite(STEPPER_PIN_3, LOW);
   digitalWrite(STEPPER_PIN_4, HIGH);
+  digitalWrite(STEPPER_PIN_5, LOW);
+  digitalWrite(STEPPER_PIN_6, LOW);
+  digitalWrite(STEPPER_PIN_7, LOW);
+  digitalWrite(STEPPER_PIN_8, HIGH);
   break;
   case 1:
   digitalWrite(STEPPER_PIN_1, LOW);
   digitalWrite(STEPPER_PIN_2, LOW);
   digitalWrite(STEPPER_PIN_3, HIGH);
   digitalWrite(STEPPER_PIN_4, LOW);
+  digitalWrite(STEPPER_PIN_5, LOW);
+  digitalWrite(STEPPER_PIN_6, LOW);
+  digitalWrite(STEPPER_PIN_7, HIGH);
+  digitalWrite(STEPPER_PIN_8, LOW);
   break;
   case 2:
   digitalWrite(STEPPER_PIN_1, LOW);
   digitalWrite(STEPPER_PIN_2, HIGH);
   digitalWrite(STEPPER_PIN_3, LOW);
   digitalWrite(STEPPER_PIN_4, LOW);
+  digitalWrite(STEPPER_PIN_5, LOW);
+  digitalWrite(STEPPER_PIN_6, HIGH);
+  digitalWrite(STEPPER_PIN_7, LOW);
+  digitalWrite(STEPPER_PIN_8, LOW);
   break;
   case 3:
   digitalWrite(STEPPER_PIN_1, HIGH);
   digitalWrite(STEPPER_PIN_2, LOW);
   digitalWrite(STEPPER_PIN_3, LOW);
   digitalWrite(STEPPER_PIN_4, LOW);
- 
+  digitalWrite(STEPPER_PIN_5, HIGH);
+  digitalWrite(STEPPER_PIN_6, LOW);
+  digitalWrite(STEPPER_PIN_7, LOW);
+  digitalWrite(STEPPER_PIN_8, LOW);
   
 } 
   }
