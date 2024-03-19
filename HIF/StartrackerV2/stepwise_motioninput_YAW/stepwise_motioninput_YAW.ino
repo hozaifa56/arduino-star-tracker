4b2911ac2c46faaa1a981b52bc65aa77963a2ffc
HOZAIFA SHAKEEL <112809008+hozaifa56@users.noreply.github.com>
2024-03-19 14:28:25 +0530

    Update stepwise_motioninput_YAW.ino

--- HIF/StartrackerV2/stepwise_motioninput_YAW/stepwise_motioninput_YAW.ino
+++ HIF/StartrackerV2/stepwise_motioninput_YAW/stepwise_motioninput_YAW.ino
@@ -1,5 +1,6 @@
 #include <Stepper.h>
-
+const int vec=20;
+int x;
 // Define the number of steps per revolution and the pins connected to the stepper motor
 const int stepsPerRevolution = 1000;
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);  // Assuming stepper motor is connected to pins 8, 9, 10, and 11
@@ -12,12 +13,14 @@ void setup() {
 
 void loop() {
   // Check if there's data available to read from the serial port
+
+
   if (Serial.available() > 0) {
     // Read the number of steps from the serial input
     int desiredSteps = Serial.parseInt();
-
+      x=desiredSteps * vec;
     // Move the stepper motor to the desired number of steps
-    myStepper.step(desiredSteps);
+    myStepper.step(x);
 
     // Optional: You might want to add a delay or perform other actions after the motor has moved
     delay(1000);  // Adjust delay as needed
